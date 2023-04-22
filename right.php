aa93581b0d045cdbf3f36849709f0e85d8219a85
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:13 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,9 @@
+<?php
+
+$this->add_right("bill", "bill", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("bill", "bill", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("bill", "bill", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("bill", "bill", "staff", view:true, add:true, edit:true);
+$this->add_right("bill", "bill", "registered", view:true, add:true);
+$this->add_right("bill", "bill", "guest", view:true, );
+
