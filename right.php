9636b8b70b00dfac559b62a2260628b68f8ce6ea
Dedan <dedanirungu@gmail.com>
2023-09-30 10:55:01 +0300

    Changes to files: right.php

--- right.php
+++ right.php
@@ -2,10 +2,9 @@
 
 /** @var \Modules\Base\Classes\Fetch\Rights $this */
 
-$this->add_right("bill", "bill", "administrator", view:true, add:true, edit:true, delete:true);
-$this->add_right("bill", "bill", "manager", view:true, add:true, edit:true, delete:true);
-$this->add_right("bill", "bill", "supervisor", view:true, add:true, edit:true, delete:true);
-$this->add_right("bill", "bill", "staff", view:true, add:true, edit:true);
-$this->add_right("bill", "bill", "registered", view:true, add:true);
-$this->add_right("bill", "bill", "guest", view:true, );
-
+$this->add_right("bill", "bill", "administrator", view: true, add: true, edit: true, delete: true);
+$this->add_right("bill", "bill", "manager", view: true, add: true, edit: true, delete: true);
+$this->add_right("bill", "bill", "supervisor", view: true, add: true, edit: true, delete: true);
+$this->add_right("bill", "bill", "staff", view: true, add: true, edit: true);
+$this->add_right("bill", "bill", "registered", );
+$this->add_right("bill", "bill", "guest", );
