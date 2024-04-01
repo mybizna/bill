108e857dc701ef91185c98d026e9704095c10c5b
Dedan <dedanirungu@gmail.com>
2024-04-01 19:28:28 +0300

    Add rights for "item" and "itemrate" to various user roles

--- right.php
+++ right.php
@@ -8,3 +8,18 @@ $this->add_right("bill", "bill", "supervisor", view: true, add: true, edit: true
 $this->add_right("bill", "bill", "staff", view: true, add: true, edit: true);
 $this->add_right("bill", "bill", "registered", );
 $this->add_right("bill", "bill", "guest", );
+
+$this->add_right("bill", "item", "administrator", view: true, add: true, edit: true, delete: true);
+$this->add_right("bill", "item", "manager", view: true, add: true, edit: true, delete: true);
+$this->add_right("bill", "item", "supervisor", view: true, add: true, edit: true, delete: true);
+$this->add_right("bill", "item", "staff", view: true, add: true, edit: true);
+$this->add_right("bill", "item", "registered", );
+$this->add_right("bill", "item", "guest", );
+
+$this->add_right("bill", "itemrate", "administrator", view: true, add: true, edit: true, delete: true);
+$this->add_right("bill", "itemrate", "manager", view: true, add: true, edit: true, delete: true);
+$this->add_right("bill", "itemrate", "supervisor", view: true, add: true, edit: true, delete: true);
+$this->add_right("bill", "itemrate", "staff", view: true, add: true, edit: true);
+$this->add_right("bill", "itemrate", "registered", );
+$this->add_right("bill", "itemrate", "guest", );
+
