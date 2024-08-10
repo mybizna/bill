12473320e557ef753dd9df632f007976686a7925
Dedan <dedanirungu@gmail.com>
2024-08-10 12:04:30 +0300

    Changes to files: database/migrations/2024_08_03_080116_create_bill_bill_table.php database/migrations/2024_08_03_080127_create_bill_item_table.php database/migrations/2024_08_03_080133_create_bill_item_rate_table.php

--- database/migrations/2024_08_03_080127_create_bill_item_table.php
+++ database/migrations/2024_08_03_080127_create_bill_item_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
             $table->id();
 
             $table->string('title');
-            $table->foreignId('bill_id');
-            $table->foreignId('ledger_id');
+            $table->foreignId('bill_id')->constrained('bill_bill')->onDelete('cascade')->index('bill_id');
+            $table->foreignId('ledger_id')->constrained('account_ledger')->onDelete('cascade')->index('ledger_id');
             $table->decimal('price', 20, 2)->default(0.00);
             $table->decimal('amount', 20, 2)->default(0.00);
             $table->string('module')->nullable();
             $table->string('model')->nullable();
-            $table->foreignId('item_id')->nullable();
+            $table->bigInteger('item_id')->nullable();
             $table->integer('quantity')->nullable();
 
             $table->timestamps();
