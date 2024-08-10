26d2b979f4eea8e677b738a03d6a663d0fd29c41
Dedan <dedanirungu@gmail.com>
2024-08-10 23:06:23 +0300

    Changes to files: Entities/Bill.php Entities/Item.php Entities/ItemRate.php

--- Entities/Item.php
+++ Entities/Item.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Modules\Bill\Entities;
+namespace Modules\Bill\Models;
 
-use Modules\Base\Entities\BaseModel;
+use Modules\Base\Models\BaseModel;
 
 class Item extends BaseModel
 {
